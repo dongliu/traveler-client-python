1136404d07a8360ade8c7be3e62c9394ee9cb5ab
Dong Liu <edongliu@gmail.com>
2021-01-02 11:39:20 -0800

    input types tested

--- tests/test_traveler.py
+++ tests/test_traveler.py
@@ -1,6 +1,6 @@
 import pytest
 import logging
-from traveler_client import client
+from traveler_client import client, traveler
 
 
 @pytest.fixture(scope='module')
@@ -11,7 +11,7 @@ def traveler_fixture():
 
 @pytest.fixture(scope='module')
 def state_fixture():
-    return {'form_id': '5fe6817cfb700100129f0231', 'traveler_id': None}
+    return {'form_id': '5ff0abd8de8f2600ef9eda2d', 'traveler_id': None, 'traveler_mapping': None, 'traveler_types': None}
 
 
 def test_create(traveler_fixture, state_fixture):
@@ -22,3 +22,7 @@ def test_create(traveler_fixture, state_fixture):
     logging.info(traveler_response_json)
     state_fixture['traveler_id'] = traveler_response_json['_id']
     assert state_fixture['traveler_id']
+    state_fixture['traveler_mapping'] = traveler_response_json['mapping']
+    assert state_fixture['traveler_mapping']
+    state_fixture['traveler_types'] = traveler_response_json['types']
+    assert state_fixture['traveler_types']
